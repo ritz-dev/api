a74a602078aefb07a451e6348c2213b0baa91bb3
dev <dev@ritz.com.mm>
2025-05-29 14:40:59 +0630

    ADD

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,5 +25,25 @@ class UserSeeder extends Seeder
             'employee_id' => 'admin',
             'remember_token' => \Illuminate\Support\Str::random(10),
         ]);
+
+        User::create([
+            'name' => 'Super Admin 1',
+            'email' => 'superadmin1@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('superpassword'), // Use a secure password
+            'role_id' => $role->id, // Assuming the role_id 1 is for 'Admin' role
+            'employee_id' => 'admin',
+            'remember_token' => \Illuminate\Support\Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Super Admin 2',
+            'email' => 'superadmin2@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('superpassword'), // Use a secure password
+            'role_id' => $role->id, // Assuming the role_id 1 is for 'Admin' role
+            'employee_id' => 'admin',
+            'remember_token' => \Illuminate\Support\Str::random(10),
+        ]);
     }
 }
