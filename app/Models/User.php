28f766e67f43e3476e78321726cfbbab2ee8a737
dev <dev@ritz.com.mm>
2025-07-07 10:01:24 +0630

    ADD

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'email',
         'password',
         'status',
-        'role_id',
+        'role_slug',
     ];
 
     protected static function boot()
